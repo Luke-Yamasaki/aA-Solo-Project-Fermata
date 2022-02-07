55784a0935f5d5d0a354eb56a03ac91151f5f0d2
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-02-07 12:32:57 -0700

    Adding AWS S3

---
+++ backend/routes/api/tracks.js
@@ -0,0 +1,8 @@
+const { ADDRGETNETWORKPARAMS } = require('dns');
+const fs = require('fs');
+const farmataStream = fs.createReadStream('big file')
+const s3 = new AWS.S3({ params: {Bucket: 'fermata-music', Key:'NfsJcEn2GPfwthOQibEGThsQ+TZU6T+tFDJtnVyq'} })
+
+s3.putObject({ Body: fermataStream }, function(err, data) {
+
+})
