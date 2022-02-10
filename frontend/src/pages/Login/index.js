58cf7be1f21d5f7ad8b850c798c1802794583a02
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-02-09 23:41:28 -0700

    Working on styling

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -10,6 +10,7 @@ export function Login() {
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
+  const [showPassword, setShowPassword] = useState(false);
 
   const userCancelBtn = document.getElementsByClassName('lgn-user-cancel-btn')[0];
   const pwrdCancelBtn = document.getElementsByClassName('lgn-pwrd-cancel-btn')[0];
@@ -19,6 +20,8 @@ export function Login() {
   const pwrdLabelDiv = document.getElementsByClassName('lgn-pwrd-label-div')[0];
   const userLabel = document.getElementsByClassName('lgn-user-label')[0];
   const pwrdLabel = document.getElementsByClassName('lgn-pwrd-label')[0];
+  const loginBtn = document.getElementsByClassName('lgn-login-btn')[0];
+  const eye = document.getElementsByClassName('lgn-show-pwrd-btn')[0];
 
   if(sessionUser) return (
     <Redirect to="/" />
@@ -50,24 +53,40 @@ export function Login() {
   }
 
   const userFocus = (e) => {
-    if(userInput.value.length > 0) {
-      userCancelBtn.style.opacity = '100%'
-    }
-    userLabelDiv.style.backgroundImage = 'linear-gradient(rgba(200, 125, 255, 1), rgba(120, 60, 220, 1));'
+    userLabelDiv.style.backgroundImage = 'linear-gradient(rgba(200, 125, 255, 1), rgba(120, 60, 220, 1))';
+    userLabelDiv.style.width = '130px';
     userLabel.style.color = "white";
+    userLabel.style.letterSpacing = "0.5px";
+    userCancelBtn.style.marginTop = "-32px"
+    userLabel.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    userInput.style.border = '5px solid rgba(200, 125, 255, 1)'
+    userInput.style.height = "40px"
+    if(userInput.value.length > 0) userCancelBtn.style.opacity = '100%'
     return
   }
 
   const pwrdFocus = (e) => {
+    pwrdLabelDiv.style.backgroundImage = 'linear-gradient(rgba(200, 125, 255, 1), rgba(120, 60, 220, 1))';
+    pwrdLabelDiv.style.width = '130px';
+    pwrdLabel.style.color = "white";
+    pwrdLabel.style.letterSpacing = "0.5px";
+    pwrdLabel.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    pwrdInput.style.border = '5px solid rgba(200, 125, 255, 1)'
+    pwrdInput.style.height = "40px"
     if(pwrdInput.value.length > 0) {
-      return pwrdCancelBtn.style.opacity = '100%'
+      pwrdCancelBtn.style.opacity = '100%'
     }
+    return
   }
 
   const userBlur = (e) => {
     e.stopPropagation();
-    userCancelBtn.style.opacity='0%';
+    userLabelDiv.style.backgroundImage = 'none';
+    userLabelDiv.style.background = 'white';
+    userLabel.style.color = 'black';
     userInput.style.border = '1px solid black';
+    userCancelBtn.style.marginTop = "-29px"
+    if(userInput.value.length === 0) userCancelBtn.style.opacity='0%';
   }
 
   const pwrdBlur = (e) => {
@@ -86,17 +105,21 @@ export function Login() {
   }
 
   const visible = () => {
-    pwrdInput.type = 'text';
-  }
-
-  const hide = () => {
-    pwrdInput.type = 'password';
+    if(!showPassword){
+      setShowPassword(true);
+      eye.style.opacity = "100%";
+      pwrdInput.type = "text";
+    } else {
+      setShowPassword(false);
+      eye.style.opacity = "50%";
+      pwrdInput.type = "password";
+    }
+    return
   }
 
   return (
     <>
       <form className="lgn-login-container" onSubmit={handleSubmit}>
-
         <h1 className="lgn-login-header">Login</h1>
         <ul className="lgn-login-error-list">
           {errors.map((error, idx) => (
@@ -104,8 +127,8 @@ export function Login() {
           ))}
         </ul>
         <div className="lgn-input-container">
-          <div className="lgn-input-box">
-            <div className="lgn-label-div">
+          <div className="lgn-user-input-box">
+            <div className="lgn-user-label-div">
               <label className="lgn-user-label">Username or Email </label>
             </div>
             <input
@@ -122,8 +145,8 @@ export function Login() {
               />
             <div className="lgn-user-cancel-btn" onClick={() => userClear()}></div>
           </div>
-          <div className="lgn-input-box">
-            <div className="lgn-label-div">
+          <div className="lgn-pwrd-input-box">
+            <div className="lgn-pwrd-label-div">
               <label className="lgn-pwrd-label">Password</label>
             </div>
             <input
@@ -131,19 +154,18 @@ export function Login() {
               type="password"
               value={password}
               placeholder="Enter password"
-              autocomplete="current-password"
+              autoComplete="current-password"
               onChange={(e) => setPassword(e.target.value)}
               onKeyUp={(e) => pwrdKeyUp(e)}
-              onfocus={(e) => pwrdFocus(e)}
-              onblur={(e) => pwrdBlur(e)}
+              onFocus={(e) => pwrdFocus(e)}
+              onBlur={(e) => pwrdBlur(e)}
               required
             />
             <div className="lgn-pwrd-cancel-btn" onClick={() => pwrdClear()}></div>
             <div className="lgn-show-pwrd-btn" onClick={() => visible()}></div>
-            <div className="lgn-hide-pwrd-btn" onClick={() => hide()}></div>
           </div>
         </div>
-        <button className="lgn-login-button" type="submit">Log In</button>
+        <button className="lgn-login-btn" type="submit">Log In</button>
       </form>
     </>
   );
