b3854d3b6bf13b3cea1abdf5ce61b1539069bcf4
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-02-12 22:13:21 -0700

    Restarting comp

--- frontend/src/pages/Discover/index.js
+++ frontend/src/pages/Discover/index.js
@@ -1,6 +1,32 @@
+import "./Discover.css";
+
 export default function Discover () {
     return(
-    <div>
+    <div className="dsc-wrapper">
+        <div className="dsc-container">
+            <div className="dsc-track-box">
+                <div className="dsc-track-image"></div>
+                <div className="dsc-track-content">
+                    <div className="dsc-track-play-info">
+                        <div className="dsc-track-play"></div>
+                        <div className="dsc-track-info">
+                            <div className="dsc-track-artist"></div>
+                            <a className="dsc-track-title" href="/tracks/:id"></a>
+                        </div>
+                        <div className="dsc-track-genre"></div>
+                    </div>
+                    <div className="dsc-track-wave-box">
+                        <div className="dsc-track-waveform"></div>
+                        <div className="dsc-track-duration-passed"></div>
+                    </div>
+                    <div className="dsc-track-owner-actions">
+                        <div className="dsc-track-edit"></div>
+                        <div className="dsc-track-delete"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
         <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink">
             <defs>
                 <linearGradient id="myGradient" className="circle-gradient" gradientTransform="rotate(90)">
@@ -12,16 +38,8 @@ export default function Discover () {
             <circle cx="5" cy="5" r="4" fill="url('#myGradient')">
                 <animate attributeType="hueRotate" values="360" dur="10s" repeatCount="indefinite" />
             </circle>
-            {/* <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink">
-            <defs>
-                <linearGradient id="myGradient" gradientTransform="rotate(90)">
-                    <stop offset="5%"  stop-color="#C77DFF" />
-                    <stop offset="95%" stop-color="#753ADB" />
-                </linearGradient>
-            </defs>
-            <circle cx="5" cy="5" r="4" fill="url('#myGradient')" />
-        </svg> */}
         </svg>
+
   </div>
   )
 }
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,45 +1,83 @@
 import "./Home.css";
 import Navbar from "../../components/Navbar";
+import Player from "../../components/Player";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="wrapper">
-        <div className="feed-container">
-          <div className="post-container">
-            <h2 className="post-title">Artists You Should Know</h2>
-            <h3 className="post-info">Top tracks form artists similar to ...</h3>
-            <div className="artists-slideshow">
-              <div className="artist-container">
-                  <div className="artist-pic"></div>
-                  <p>Artist Name</p>
-                  <p>Top tracks</p>
+      <div className="hme-wrapper">
+        <div className="hme-container">
+          <div className="hme-track-box">
+            <div className="hme-track-image"></div>
+            <div className="hme-track-content">
+              <div className="hme-track-play-info">
+                  <div className="hme-track-play"></div>
+                  <div className="hme-track-info">
+                      <div className="hme-track-artist"></div>
+                      <a className="hme-track-title" href="/tracks/:id"></a>
+                  </div>
+                  <div className="hme-track-genre"></div>
               </div>
-              <div className="artist-container">
-                  <div className="artist-pic"></div>
-                  <p>Artist Name</p>
-                  <p>Top tracks</p>
+              <div className="hme-track-wave-box">
+                  <div className="hme-track-waveform"></div>
+                  <div className="hme-track-duration-passed"></div>
               </div>
-              <div className="artist-container">
-                  <div className="artist-pic"></div>
-                  <p>Artist Name</p>
-                  <p>Top tracks</p>
-              </div>
-              <div className="artist-container">
-                  <div className="artist-pic"></div>
-                  <p>Artist Name</p>
-                  <p>Top tracks</p>
-              </div>
-              <div className="artist-container">
-                  <div className="artist-pic"></div>
-                  <p>Artist Name</p>
-                  <p>Top tracks</p>
+              <div className="hme-track-owner-actions">
+                  <div className="hme-track-edit"></div>
+                  <div className="hme-track-delete"></div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <Player />
     </>
   );
+}
+
+import "./Discover.css";
+
+export default function Discover () {
+    return(
+    <div className="hme-wrapper">
+        <div className="hme-container">
+            <div className="sc-track-box">
+                <div className="hme-track-image"></div>
+                <div className="hme-track-content">
+                    <div className="hme-track-play-info">
+                        <div className="hme-track-play"></div>
+                        <div className="hme-track-info">
+                            <div className="hme-track-artist"></div>
+                            <a className="hme-track-title" href="/tracks/:id"></a>
+                        </div>
+                        <div className="hme-track-genre"></div>
+                    </div>
+                    <div className="hme-track-wave-box">
+                        <div className="hme-track-waveform"></div>
+                        <div className="hme-track-duration-passed"></div>
+                    </div>
+                    <div className="hme-track-owner-actions">
+                        <div className="hme-track-edit"></div>
+                        <div className="hme-track-delete"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink">
+            <defs>
+                <linearGradient id="myGradient" className="circle-gradient" gradientTransform="rotate(90)">
+                    <stop offset="0%" stop-color="yellow" />
+                    <stop offset="95%"  stop-color="#C77DFF" />
+                    {/* <stop offset="95%" stop-color="pink" /> */}
+                </linearGradient>
+            </defs>
+            <circle cx="5" cy="5" r="4" fill="url('#myGradient')">
+                <animate attributeType="hueRotate" values="360" dur="10s" repeatCount="indefinite" />
+            </circle>
+        </svg>
+
+  </div>
+  )
 }
