18dddf437991c58c21742db9c63625e73c4b528e
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-02-12 21:53:46 -0700

    Adding player component

--- frontend/src/components/Player/index.js
+++ frontend/src/components/Player/index.js
@@ -1,20 +1,26 @@
 import "./Player.css"
 
-export function Player () {
+export function Player ({ track }) {
+    const wave = track;
+    const duration = track;
+    const artist = track;
+    const title = track;
+    const image = track;
+
     return (
-        <div className="">
-            <button></button>
-            <button></button>
-            <div>
-                <div>Remaining</div>
-                <div>Progress bar</div>
-                <div>Duration</div>
+        <div className="plyr-container">
+            <button className="plyr-play-btn"></button>
+            <button className="plyr-stop-btn"></button>
+            <div className="plyr-playback-info">
+                <div className="plyr-current-duration">Remaining</div>
+                <div className="plyr-progress-bar">Progress bar</div>
+                <div className="plyr-total-duration">Duration</div>
             </div>
             <div className='plyr-slider-container'>Slider
                 <input type="range" min="0" max='100' value='50' className='plyr-volume-slider'></input>
             </div>
             <div className="plyr-track-box">
-                <img className='plyr-track-image'></img>
+                <div className='plyr-track-image'></div>
                 <div className="plyr-track-info">
                     <a className='plyr-track-artist' href="/users/:id"></a>
                     <a className="plyr-track-title" href="/tracks/:id"></a>
